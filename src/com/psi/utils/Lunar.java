6dbe5d68fbd9f19f9de7ee0edb838daa1cccc9ff
Liujiuwu <liujiuwu@gmail.com>
2009-10-22 20:02:03 +0800

    modify Lunar.java

--- src/com/psi/utils/Lunar.java
+++ src/com/psi/utils/Lunar.java
@@ -19,8 +19,9 @@ public class Lunar {
 	final private static int yearDays(int y) {
 		int i, sum = 348;
 		for (i = 0x8000; i > 0x8; i >>= 1) {
-			if ((lunarInfo[y - 1900] & i) != 0)
+			if ((lunarInfo[y - 1900] & i) != 0){
 				sum += 1;
+			}
 		}
 		return (sum + leapDays(y));
 	}
