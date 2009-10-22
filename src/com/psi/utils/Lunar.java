0bda607942de4b5c6d29b53b90124f90be043591
Liujiuwu <liujiuwu@gmail.com>
2009-10-22 20:02:29 +0800

    modify Lunar.java

--- src/com/psi/utils/Lunar.java
+++ src/com/psi/utils/Lunar.java
@@ -29,10 +29,11 @@ public class Lunar {
 	//====== 传回农历 y年闰月的天数
 	final private static int leapDays(int y) {
 		if (leapMonth(y) != 0) {
-			if ((lunarInfo[y - 1900] & 0x10000) != 0)
+			if ((lunarInfo[y - 1900] & 0x10000) != 0){
 				return 30;
-			else
+			}else{
 				return 29;
+			}
 		} else{
 			return 0;
 		}
